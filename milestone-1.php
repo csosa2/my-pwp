370eb17365935fe22d6974172d03a963abf1dfc8
Christina Sosa <csosa2106@gmail.com>
2016-07-20 14:51:44 -0600

    made final changes before submitting for milestone 1 check in

--- milestone-1.php
+++ milestone-1.php
@@ -12,42 +12,40 @@
 			<h2>Purpose:</h2>
 			<p>The purpose of this website is to showcase my skillset, past work and personality to future employers,
 				potential clients and fellow colleagues in the tech industry. This website will be used as a portfolio
-				and resume showing my skills.</p>
+				and resume to show my skills.</p>
 			<h2>Audience:</h2>
 			<p>The audience will include professional colleagues, potential employers and possible clients.</p>
 			<h2>Goal:</h2>
 			<p>The goal is to acquire a full time employment with a local company, work as a freelancer with small
-				business clientele and working with fellow developers on events, projects and opportunities locally to
-				expand my skills for a senior developer position.</p>
+				business clientele and work with fellow developers on different endeavours.</p>
 			<h2>Persona:</h2>
 			<h3>Name:</h3>
 			<p>Jane Smith</p>
 			<h3>Age:</h3>
 			<p>35</p>
 			<h3>Role:</h3>
-			<p>Manager at RiskSence in Albuquerque, NM</p>
+			<p>Manager at RiskSense in Albuquerque, NM</p>
 			<h3>Technology:</h3>
 			<p>Jane is very comfortable with technology and describes her computer skills as advanced. For work, she has
 				a Lenovo ThinkPad P40 Yoga laptop that docks into her dual screen station at the office. When not on her
 				laptop, Jane uses her Nexus 6P to check emails on the go. Jane loves using Google products and uses them to
-				communicate and work during team projects.
-				computer is an HP</p>
+				communicate and work during team projects.</p>
 			<h3>Behaviors and Attitudes:</h3>
-			<p>Jane started at RiskSense five years ago and doensn't plan on leaving any time soon. She loves her job!
+			<p>Jane started at RiskSense five years ago and doesn't plan on leaving any time soon. She loves her job!
 				She started out as a front end developer with the company. Within five years, she has been promoted to a
-				manager position with her own team. Jane is very happy leading her own team, and values the imput of all
+				manager position with her own team. Jane is very happy leading her own team, and values the input of all
 				the members on her team. She always has a can-do attitude and helps alleviate stress during crunch time
-				on projects. Janes best attribute, she always keeps calm under stress. </p>
+				on projects. Jane's best attribute, she always keeps calm under stress. </p>
 			<h3>Scenario:</h3>
 			<p>Jane has been a manager with RiskSense for five years. She has worked for a few tech companies in
 				Albuquerque before finding her current position at RiskSense. Jane realizes that her front end
 				development team is getting a little overwhelmed with work and asks if the company can hire another
-				full-time developer to help alleviate the stress of a growing company. After
-				approval and posting, Jane will be involved in the interview process to help search for the perfect
-				addition to her front end development team.</p>
-			<h3>Goal(s):</h3>
-			<p>  Jane is determined to find the best person to join her front end development team. With limited time on
-				her hands, she hopes to find some potential candidates through social media, community meetups and
+				full-time developer to help alleviate the stress of a growing company. After approval and posting, Jane will
+				be involved in the interview process to help search for the perfect addition to her front end development
+				team.</p>
+			<h3>Goal:</h3>
+			<p> Jane is determined to find the best person to join her front end development team. With limited time on
+				her hands, she hopes to find some potential candidates through social media, community meet-ups and
 				looking through professional websites.
 			</p>
 			<h1>Use Case:</h1>
@@ -57,19 +55,23 @@
 				imaginative UX/UI designs.
 			</p>
 			<ol>
-				<li>Jane looks over Christina Sosa's application and notices a professional website noted on her resume
-					.</li>
+				<li>Jane looks over Christina Sosa's application and remembers her from a meet-up they attended last week.
+					With excitement, she notices a professional website noted on her resume.
+				</li>
 				<li>Jane gets on her Lenovo ThinkPad P40 and opens up a Chrome browswer.</li>
 				<li>She types in christinasosa.com</li>
-				<li>The website opens and Jane finds a visually interesting home page with buttons buttons along the
-					header of the page.</li>
-				<li>Jane clicks around to "About", "Portfolio" and "Resume" to learn more about Christina Sosa and her
-					skills.
+				<li>The website opens and Jane finds a visually interesting home page with buttons along the
+					header of the page.
+				</li>
+				<li>Jane clicks around the web site to "About", "Portfolio" and "Resume" to learn more about Christina
+					Sosa and her skills.
 				</li>
 				<li>Jane is very excited to see all the skills Christina learned during her recent attendance at the
-					Deep Dive Coding Bootcamp in Albuquerque, NM.</li>
+					Deep Dive Coding Bootcamp in Albuquerque, NM.
+				</li>
 				<li>Jane proceeds to inform the HR representative that she is very interested in moving forward with
-					Christina Sosa's application and would like to schedule an interview.</li>
+					Christina Sosa's application and would like to schedule an interview.
+				</li>
 			</ol>
 		</main>
 		<footer>
