227b78225e1dbed326547790371e5a42db3654e7
Christina Sosa <csosa2106@gmail.com>
2016-07-20 14:13:23 -0600

    Added persona section

--- milestone-1.php
+++ milestone-1.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
 		<title>My Personal Web Site</title>
@@ -21,16 +21,60 @@
 				expand my skills for a senior developer position.</p>
 			<h2>Persona:</h2>
 			<h3>Name:</h3>
+			<p>Jane Smith</p>
 			<h3>Age:</h3>
+			<p>35</p>
 			<h3>Role:</h3>
+			<p>Manager at RiskSence in Albuquerque, NM</p>
 			<h3>Technology:</h3>
+			<p>Jane is very comfortable with technology and describes her computer skills as advanced. For work, she has
+				a Lenovo ThinkPad P40 Yoga laptop that docks into her dual screen station at the office. When not on her
+				laptop, Jane uses her Nexus 6P to check emails on the go. Jane loves using Google products and uses them to
+				communicate and work during team projects.
+				computer is an HP</p>
 			<h3>Behaviors and Attitudes:</h3>
+			<p>Jane started at RiskSense five years ago and doensn't plan on leaving any time soon. She loves her job!
+				She started out as a front end developer with the company. Within five years, she has been promoted to a
+				manager position with her own team. Jane is very happy leading her own team, and values the imput of all
+				the members on her team. She always has a can-do attitude and helps alleviate stress during crunch time
+				on projects. Janes best attribute, she always keeps calm under stress. </p>
 			<h3>Scenario:</h3>
+			<p>Jane has been a manager with RiskSense for five years. She has worked for a few tech companies in
+				Albuquerque before finding her current position at RiskSense. Jane realizes that her front end
+				development team is getting a little overwhelmed with work and asks if the company can hire another
+				full-time developer to help alleviate the stress of a growing company. After
+				approval and posting, Jane will be involved in the interview process to help search for the perfect
+				addition to her front end development team.</p>
 			<h3>Goal(s):</h3>
-
+			<p>  Jane is determined to find the best person to join her front end development team. With limited time on
+				her hands, she hopes to find some potential candidates through social media, community meetups and
+				looking through professional websites.
+			</p>
+			<h1>Use Case:</h1>
+			<h2>Goal:</h2>
+			<p>Jane wants to find a junior developer with advanced front end skills that show in his/her professoinal
+				website. She wants to see a professional website that demonstrates robust (current) coding skills with
+				imaginative UX/UI designs.
+			</p>
+			<ol>
+				<li>Jane looks over Christina Sosa's application and notices a professional website noted on her resume
+					.</li>
+				<li>Jane gets on her Lenovo ThinkPad P40 and opens up a Chrome browswer.</li>
+				<li>She types in christinasosa.com</li>
+				<li>The website opens and Jane finds a visually interesting home page with buttons buttons along the
+					header of the page.</li>
+				<li>Jane clicks around to "About", "Portfolio" and "Resume" to learn more about Christina Sosa and her
+					skills.
+				</li>
+				<li>Jane is very excited to see all the skills Christina learned during her recent attendance at the
+					Deep Dive Coding Bootcamp in Albuquerque, NM.</li>
+				<li>Jane proceeds to inform the HR representative that she is very interested in moving forward with
+					Christina Sosa's application and would like to schedule an interview.</li>
+			</ol>
 		</main>
-	</body>
-	<footer>
+		<footer>
 
-	</footer>
+		</footer>
+	</body>
+
 </html>
