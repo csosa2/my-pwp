13c47054c2f15df6f1b9f5aba8d680e409aafebc
Christina Sosa <csosa2106@gmail.com>
2016-07-20 09:44:08 -0600

    test

---
+++ milestone-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>My Personal Web Site</title>
+	</head>
+	<body>
+		<header>
+			<h1>Christina Sosa: Personal Web Site</h1>
+		</header>
+		<main>
+			<h2>Purpose:</h2>
+			<p>The purpose of this website is to showcase my skillset, past work and personality to future employers,
+				potential clients and fellow colleagues in the tech industry. This website will be used as a portfolio
+				and resume showing my skills.</p>
+			<h2>Audience:</h2>
+			<p>The audience will include professional colleagues, potential employers and possible clients.</p>
+			<h2>Goal:</h2>
+			<p>The goal is to acquire a full time employment with a local company, work as a freelancer with small
+				business clientele and working with fellow developers on events, projects and opportunities locally to
+				expand my skills for a senior developer position.</p>
+			<h2>Persona:</h2>
+			<h3>Name:</h3>
+			<h3>Age:</h3>
+			<h3>Role:</h3>
+			<h3>Technology:</h3>
+			<h3>Behaviors and Attitudes:</h3>
+			<h3>Scenario:</h3>
+			<h3>Goal(s):</h3>
+
+		</main>
+	</body>
+	<footer>
+
+	</footer>
+</html>
